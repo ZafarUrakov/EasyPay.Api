b0c07f9a593762d7901f26d5b654083446305682
Zafar <icom@MACKBOOK>
2023-10-10 21:01:39 +0300

    INFRA: Initialize Unit Test

---
+++ EasyPay.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,15 @@
+//===========================
+// Copyright (c) Tarteeb LLC
+// Manage Your Money Easy
+//===========================
+
+using Xunit;
+
+namespace EasyPay.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
