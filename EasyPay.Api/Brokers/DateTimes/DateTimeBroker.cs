12519d882a6d1d39d65432b8d1698857b448eb6f
Zafar <icom@MACKBOOK>
2023-10-11 14:17:55 +0300

    BROKERS:  Add Date Time Broker

---
+++ EasyPay.Api/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,15 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Manage Your Money Easy
+//===========================
+
+using System;
+
+namespace EasyPay.Api.Brokers.DateTimes
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTimeOffset() =>
+            DateTimeOffset.UtcNow;
+    }
+}
