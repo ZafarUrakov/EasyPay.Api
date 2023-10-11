658f35efa581db0c5d89ea32bd8772795cdae1f2
Zafar <icom@MACKBOOK>
2023-10-11 11:26:17 +0300

    BROKERS: Insert Account

--- EasyPay.Api/Brokers/Storages/StorageBroker.Accounts.cs
+++ EasyPay.Api/Brokers/Storages/StorageBroker.Accounts.cs
@@ -5,11 +5,15 @@
 
 using EasyPay.Api.Models.Accounts;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace EasyPay.Api.Brokers.Storages
 {
     public partial class StorageBroker
     {
         public DbSet<Account> Accounts { get; set; }
+
+        public async ValueTask<Account> InsertAccountAsync(Account account) =>
+            await InsertAsync(account);
     }
 }
