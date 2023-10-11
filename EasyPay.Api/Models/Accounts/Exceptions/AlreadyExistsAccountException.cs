5b6b7af5c3ba1e9bea99780e9a1488f67ccf5416
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-10-11 21:21:20 +0900

    POC: Add Account (WIP)

---
+++ EasyPay.Api/Models/Accounts/Exceptions/AlreadyExistsAccountException.cs
@@ -0,0 +1,18 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Manage Your Money Easy
+//===========================
+
+using System;
+using Xeptions;
+
+namespace EasyPay.Api.Models.Accounts.Exceptions
+{
+    public class AlreadyExistsAccountException : Xeption
+    {
+        public AlreadyExistsAccountException(Exception innerException)
+            : base(message: "Account already exists",
+                  innerException)
+        { }
+    }
+}
---
+++ EasyPay.Api/Services/Foundations/Accounts/AccountService.Exceptions.cs
@@ -0,0 +1,112 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Manage Your Money Easy
+//===========================
+
+using System;
+using System.Threading.Tasks;
+using EasyPay.Api.Models.Accounts;
+using EasyPay.Api.Models.Accounts.Exceptions;
+using EFxceptions.Models.Exceptions;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Xeptions;
+
+namespace EasyPay.Api.Services.Foundations.Accounts
+{
+    public partial class AccountService
+    {
+        private delegate ValueTask<Account> ReturningAccountFunction();
+
+        private ValueTask<Account> TryCatch(ReturningAccountFunction returningAccountFunction)
+        {
+            try
+            {
+                return returningAccountFunction();
+            }
+            catch (AccountNotNull accountNotNull)
+            {
+                throw CreateAndLogValidationException(accountNotNull);
+            }
+            catch (InvalidAccountException invalidAccountException)
+            {
+                throw CreateAndLogValidationException(invalidAccountException);
+            }
+            catch(DuplicateKeyException duplicateKeyException)
+            {
+                var alreadyExistsAccountException =
+                    new AlreadyExistsAccountException(duplicateKeyException);
+
+                throw CreateAndLogDependencyValidationException(alreadyExistsAccountException);
+            }
+            catch(SqlException sqlException)
+            {
+                var failedStorageAccountException =
+                    new FailedStorageAccountException(sqlException);
+
+                throw CreateAndLogAccountServiceException(failedStorageAccountException);
+            }
+            catch(DbUpdateConcurrencyException dbUpdateConcurrencyException)
+            {
+                var lockedAccountException =
+                    new LockedAccountException(dbUpdateConcurrencyException);
+
+                throw CreateAndLogDependencyException(lockedAccountException);
+            }
+            catch(DbUpdateException dbUpdateException)
+            {
+                var failedStorageAccountException =
+                    new FailedStorageAccountException(dbUpdateException);
+
+                throw CreateAndLogCriticalDependencyException(failedStorageAccountException);
+            }
+            catch(Exception exception)
+            {
+                var failedAccountServiceException =
+                    new FailedAccountServiceException(exception);
+
+                throw CreateAndLogAccountServiceException(failedAccountServiceException);
+            }
+        }
+
+        private AccountValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var accountValidationException = new AccountValidationException(exception);
+            this.loggingBroker.LogCritical(accountValidationException);
+
+            return accountValidationException;
+        }
+
+        private AccountDependencyValidationException CreateAndLogDependencyValidationException(Xeption exception)
+        {
+            var accountDependencyValidationException = new AccountDependencyValidationException(exception);
+            this.loggingBroker.LogError(accountDependencyValidationException);
+
+            return accountDependencyValidationException;
+        }
+
+        private AccountDependencyException CreateAndLogDependencyException(Xeption exception)
+        {
+            var accountDependencyException = new AccountDependencyException(exception);
+            this.loggingBroker.LogError(accountDependencyException);
+
+            return accountDependencyException;
+        }
+
+        private AccountDependencyException CreateAndLogCriticalDependencyException(Xeption exception)
+        {
+            var accountDependencyException = new AccountDependencyException(exception);
+            this.loggingBroker.LogCritical(accountDependencyException);
+
+            return accountDependencyException;
+        }
+
+        private AccountServiceException CreateAndLogAccountServiceException(Xeption exception)
+        {
+            var accountServiceException = new AccountServiceException(exception);
+            this.loggingBroker.LogError(accountServiceException);
+
+            return accountServiceException;
+        }
+    }
+}
