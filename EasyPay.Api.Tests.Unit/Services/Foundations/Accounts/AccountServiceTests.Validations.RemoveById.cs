3d1fde31d82a33dacd1cbbc23c162367e04b7206
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-10-18 19:21:14 +0900

    ShouldThrowNotFoundExceptionOnRemoveByIdIfAccountIsNotFoundAndLogItAsync -> FAIL

--- EasyPay.Api.Tests.Unit/Services/Foundations/Accounts/AccountServiceTests.Validations.RemoveById.cs
+++ EasyPay.Api.Tests.Unit/Services/Foundations/Accounts/AccountServiceTests.Validations.RemoveById.cs
@@ -49,5 +49,41 @@ namespace EasyPay.Api.Tests.Unit.Services.Foundations.Accounts
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowNotFoundExceptionOnRemoveByIdIfAccountIsNotFoundAndLogItAsync()
+        {
+            //given
+            Guid randomAccountId = Guid.NewGuid();
+            Guid inputAccountId = randomAccountId;
+            Account noAccount = null;
+
+            var notFoundAccountException = new NotFoundAccountException(inputAccountId);
+
+            var expectedAccountValidationException =
+                new AccountValidationException(notFoundAccountException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAccountByIdAsync(inputAccountId)).ReturnsAsync(noAccount);
+
+            //when
+            ValueTask<Account> removeAccountByidTask =
+                this.accountService.RemoveAccountByIdAsync(inputAccountId);
+
+            AccountValidationException actualAccountValidationException =
+                await Assert.ThrowsAsync<AccountValidationException>(removeAccountByidTask.AsTask);
+
+            //then
+            actualAccountValidationException.Should().BeEquivalentTo(expectedAccountValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAccountByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedAccountValidationException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
