1921bac8f8ac4df4356b9b10a906c4d5442a4d7b
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-10-21 20:42:50 +0900

    DATA: Transfer Model & Migrate

---
+++ EasyPay.Api/Models/Transfers/Transfer.cs
@@ -0,0 +1,18 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Manage Your Money Easy
+//===========================
+
+using System;
+using EasyPay.Api.Models.Accounts;
+
+namespace EasyPay.Api.Models.Transfers
+{
+    public class Transfer
+    {
+        public Guid TransferId { get; set; }
+        public decimal Amount { get; set; }
+        public Account Accounts { get; set; }
+        public string ReceiverAccountNumber { get; set; }
+    }
+}
