54edfaecb9cfa38b51d999249ab58a405c36bc01
Zafar <icom@MACKBOOK>
2023-10-22 16:08:18 +0300

    POC: Add Transfer(WIP)

---
+++ EasyPay.Api/Models/Accounts/Exceptions/NotFoundAccountByAccountNumberException.cs
@@ -0,0 +1,16 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Manage Your Money Easy
+//===========================
+
+using Xeptions;
+
+namespace EasyPay.Api.Models.Accounts.Exceptions
+{
+    public class NotFoundAccountByAccountNumberException : Xeption
+    {
+        public NotFoundAccountByAccountNumberException(string accountNumber)
+            : base(message: $"Account is not found with number: {accountNumber}")
+        { }
+    }
+}
---
+++ EasyPay.Api/Services/Foundations/Transfers/TransferService.Validation.cs
@@ -0,0 +1,95 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Manage Your Money Easy
+//===========================
+
+using EasyPay.Api.Models.Accounts;
+using EasyPay.Api.Models.Accounts.Exceptions;
+using EasyPay.Api.Models.Transfers;
+using EasyPay.Api.Models.Transfers.Exceptions;
+using System;
+
+namespace EasyPay.Api.Services.Foundations.Transfers
+{
+    public partial class TransferService
+    {
+        public void ValidateTransferOnAdd(Transfer transfer)
+        {
+            Validate(
+                (Rule: IsInvalid(transfer.TransferId), Parameter: nameof(Transfer.TransferId)),
+                (Rule: IsInvalid(transfer.SourceAccountNumber), Parameter: nameof(Transfer.SourceAccountNumber)),
+                (Rule: IsInvalid(transfer.ReceiverAccountNumber), Parameter: nameof(Transfer.ReceiverAccountNumber)),
+                (Rule: IsInvalid(transfer.Amount), Parameter: nameof(Transfer.Amount)));
+        }
+
+        public void ValidateTransferOnAddByDeposit(Transfer transfer)
+        {
+            Validate(
+               (Rule: IsInvalid(transfer.SourceAccountNumber), Parameter: nameof(Transfer.SourceAccountNumber)),
+               (Rule: IsInvalid(transfer.Amount), Parameter: nameof(Transfer.Amount)));
+        }
+
+        private static dynamic IsInvalid(Guid trnasferId) => new
+        {
+            Condition = trnasferId == default,
+            Message = "Id is required"
+        };
+
+        private static dynamic IsInvalid(string text) => new
+        {
+            Condition = String.IsNullOrWhiteSpace(text),
+            Message = "Text is required"
+        };
+
+        private static dynamic IsInvalid(decimal amount) => new
+        {
+            Condition = amount == default,
+            Message = "Amount is required"
+        };
+
+        private static void ValidateAccountNotFoundForTransfer(Account sourceAccount, Account recieverAccount)
+        {
+            if (sourceAccount == null)
+            {
+                throw new NotFoundAccountByAccountNumberException(sourceAccount.AccountNumber);
+            }
+            if (recieverAccount == null)
+            {
+                throw new NotFoundAccountByAccountNumberException(recieverAccount.AccountNumber);
+            }
+        }
+
+        private static void ValidateAccountNotFoundForTransfer(Account account)
+        {
+            if (account == null)
+            {
+                throw new NotFoundAccountByAccountNumberException(account.AccountNumber);
+            }
+        }
+
+        private static void ValidateAccountInsufficientFunds(decimal amount, Account sourceAccount)
+        {
+            if (sourceAccount.Balance < amount)
+            {
+                throw new InsufficientFundsException();
+            }
+        }
+
+        private static void Validate(params (dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidClientException = new InvalidTransferException();
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidClientException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidClientException.ThrowIfContainsErrors();
+        }
+    }
+}
