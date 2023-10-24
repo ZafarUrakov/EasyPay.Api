0f7defef519b8340bc33c417d8ed4652ff84a92c
Zafar <icom@MACKBOOK>
2023-10-24 22:06:22 +0300

    DATA: Update Client

---
+++ EasyPay.Api/Migrations/20231024172934_UpdateClient.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyPay.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateClient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Login",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Login",
+                table: "Clients");
+        }
+    }
+}
