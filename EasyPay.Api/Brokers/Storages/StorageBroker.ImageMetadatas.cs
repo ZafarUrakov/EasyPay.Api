195327dd3363e183883897876c52f22398111967
Zafar <icom@MACKBOOK>
2023-10-26 16:49:07 +0300

    DATA: Model & Migrate ImageMetadata

---
+++ EasyPay.Api/Brokers/Storages/StorageBroker.ImageMetadatas.cs
@@ -0,0 +1,15 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Manage Your Money Easy
+//===========================
+
+using EasyPay.Api.Models.Images;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyPay.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<ImageMetadata> ImageMetadatas { get; set; }
+    }
+}
---
+++ EasyPay.Api/Models/ImageMetadatas/ImageType.cs
@@ -0,0 +1,17 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Manage Your Money Easy
+//===========================
+
+namespace EasyPay.Api.Models.Images
+{
+    public enum ImageType
+    {
+        png,
+        jpeg,
+        gif,
+        svg,
+        jpg,
+
+    }
+}
