05d0d6921b043e6332fcbc87805993ab14b5816f
Zafar <icom@MACKBOOK>
2023-10-26 17:10:55 +0300

    FOUNDATIONS: CRUD Imaga Metadata

---
+++ EasyPay.Api/Services/Foundations/ImagaMetadatas/IImageMetadataService.cs
@@ -0,0 +1,22 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Manage Your Money Easy
+//===========================
+
+using EasyPay.Api.Models.Clients;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using EasyPay.Api.Models.Images;
+
+namespace EasyPay.Api.Services.Foundations.ImagaMetadatas
+{
+    public interface IImageMetadataService
+    {
+        ValueTask<ImageMetadata> AddClientAsync(ImageMetadata imageMetadata);
+        IQueryable<ImageMetadata> RetrieveAllClients();
+        ValueTask<ImageMetadata> RetrieveClientByIdAsync(Guid clientId);
+        ValueTask<ImageMetadata> RemoveClientByIdAsync(Guid locationId);
+        ValueTask<ImageMetadata> ModifyClientAsync(ImageMetadata imageMetadata);
+    }
+}
