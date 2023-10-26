5699c1f1d43cc2e51bdb5994ce87b2a67222ede9
Zafar <icom@MACKBOOK>
2023-10-26 16:53:03 +0300

    BROKERS: Image Metadata CRUD

--- EasyPay.Api/Brokers/Storages/StorageBroker.ImageMetadatas.cs
+++ EasyPay.Api/Brokers/Storages/StorageBroker.ImageMetadatas.cs
@@ -3,13 +3,32 @@
 // Manage Your Money Easy
 //===========================
 
+using EasyPay.Api.Models.Clients;
 using EasyPay.Api.Models.Images;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
 
 namespace EasyPay.Api.Brokers.Storages
 {
     public partial class StorageBroker
     {
         public DbSet<ImageMetadata> ImageMetadatas { get; set; }
+
+        public async ValueTask<ImageMetadata> InsertImageMetadataAsync(ImageMetadata imageMetadata) =>
+            await InsertAsync(imageMetadata);
+
+        public IQueryable<ImageMetadata> SelectAllImageMetadatas() =>
+            SelectAll<ImageMetadata>();
+
+        public async ValueTask<ImageMetadata> SelectImageMetadataByIdAsync(Guid imageMetadataId) =>
+            await SelectAsync<ImageMetadata>(imageMetadataId);
+
+        public async ValueTask<ImageMetadata> UpdateImageMetadataAsync(ImageMetadata imageMetadata) =>
+            await UpdateAsync(imageMetadata);
+
+        public async ValueTask<ImageMetadata> DeleteImageMetadataAsync(ImageMetadata imageMetadata) =>
+            await DeleteAsync(imageMetadata);
     }
 }
